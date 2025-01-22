c4adedbd3917fc8644a58bb6250cd5c0e2f09153
Brad Ziegler <bradleyziegler72@gmail.com>
2025-01-22 00:31:12 -0500

    Added some remote action methods around changing display names along with a bit of server authority to limit what can be submitted.

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,17 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour {
-    [SyncVar] [SerializeField] private string displayName = "Missing Name";
-    [SyncVar] [SerializeField] private Color displayColor = Color.black;
+    [SerializeField] private TMP_Text displayNameText = null;
+    [SerializeField] private Renderer displayColorRenderer = null;
 
-    [Server] public void SetDisplayName(string newDisplayName) {
-        displayName = newDisplayName;
-    }
+    [SyncVar(hook=nameof(HandleDisplayNameUpdate))]
+    [SerializeField] private string displayName = "Missing Name";
 
-    [Server] public void SetDisplayColor(Color newPlayerColor) {
-        displayColor = newPlayerColor;
-    }
+    [SyncVar(hook=nameof(HandleDisplayColorUpdated))] 
+    [SerializeField] private Color displayColor = Color.black;
+
+    #region Server
+
+        [Server] public void SetDisplayName(string newDisplayName) {
+            displayName = newDisplayName;
+        }
+
+        [Server] public void SetDisplayColor(Color newPlayerColor) {
+            displayColor = newPlayerColor;
+        }
+
+        [Command] private void CmdSetDisplayName(string newDisplayName) {
+            if (newDisplayName.Length < 2 || newDisplayName.Length > 20) { return; }
+            RpcLogNewName(displayName);
+            SetDisplayName(newDisplayName);
+        }
+
+    #endregion
+
+    #region Client
+
+        private void HandleDisplayNameUpdate(string oldName, string newName) {
+            displayNameText.text = newName;
+        }
+
+        private void HandleDisplayColorUpdated(Color oldColor, Color newColor) {
+            displayColorRenderer.material.SetColor("_BaseColor", newColor);
+        }
+
+        [ContextMenu("Set My Name")]
+        private void SetMyName() {
+            CmdSetDisplayName("My New Name");
+        }
+
+        [ClientRpc] private void RpcLogNewName(string newDisplayName) {
+            Debug.Log(newDisplayName);
+        }
+
+    #endregion
 }
